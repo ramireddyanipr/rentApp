85acc2d5265d928bc2c710fd98584b5ac659aad8
suryamuppalla <surya@anipr.in>
2016-02-19 10:34:33 +0530

    added overdue payment

--- www/js/services/admin/admin.js
+++ www/js/services/admin/admin.js
@@ -7,7 +7,6 @@ appServices.factory('dashBoard', function ($q, $timeout) {
     };
     
     dashBoard.prototype.getCollectedPayment = function (user, sDate, eDate) {
-        
         // body...
         var def = $q.defer();
         var result = [];
@@ -33,8 +32,8 @@ appServices.factory('dashBoard', function ($q, $timeout) {
             }
             
             query.limit(1000); 
-            // query.lessThan("dueDate", eDate);
-            // query.greaterThan("dueDate", sDate);
+            query.lessThan("dueDate", eDate);
+            query.greaterThan("dueDate", sDate);
 
             query.include('tenantPointer');
             query.include('tenantProperty')
@@ -52,14 +51,48 @@ appServices.factory('dashBoard', function ($q, $timeout) {
             });
         }
         process(false);
-        
-        this.load = true;
-        
-        console.log(def.promise)
         return def.promise
     }; // end
     
-    console.log(dashBoard);
+    dashBoard.prototype.overDuePayment = function (user) {
+        // body...
+        var def = $q.defer()
+        var result = [];
+
+        var processCallback = function(res) {
+            result = result.concat(res);
+            if (res.length == 1000) {
+              process(res[res.length-1].id);
+              return;
+            }
+
+            def.resolve(result)
+        }
+
+        var process = function(skip) {
+            var FuturePayments = Parse.Object.extend('FuturePayments');
+            var query = new Parse.Query(FuturePayments);
+
+            if (skip) {
+              query.greaterThan("objectId", skip);
+            }
+
+            query.limit(1000); 
+            query.equalTo('status', 'pending')
+
+            query.include('tenantPointer');
+            query.include('tenantProperty')
+            query.include('tenantProperty.property.owner')
+            query.include('tenantProperty.property.managerPointer')
+            query.include('tenant')
+
+            query.ascending("objectId");
+            query.find().then(function (res) { processCallback(res) }, function queryFailed(error) {console.log(error); def.reject(error)});
+        }
+        process(false);
+        return def.promise
+    }; // end over due payment
+    
     return dashBoard;
 });
 
--- www/templates/overdue-payment/overdue-payment.js
+++ www/templates/overdue-payment/overdue-payment.js
@@ -1,6 +1,18 @@
 
-appControllers.controller('overduePaymentCtrl', function ($scope,$mdMedia,$mdDialog,$mdToast,                    $mdBottomSheet, $q, dashBoard) {
+appControllers.controller('overduePaymentCtrl', function ($scope,$mdMedia,$mdDialog,$mdToast,                    $mdBottomSheet, $q, dashBoard, $timeout) {
 
-
+    
+    var dashboard = new dashBoard();
+    $scope.load = dashboard.overDuePayment().then (function (success) {
+        console.log(success);
+        $scope.overDue = JSON.parse(JSON.stringify(success));
+        
+        $timeout (function () {$scope.load = false;}, 1000);
+        
+        console.log($scope.overDue);
+    }, function (error) {
+        console.log(error);
+    });
+    
 
 });// End of controller menu dashboard.
